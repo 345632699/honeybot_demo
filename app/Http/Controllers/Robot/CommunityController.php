fa6d48ee85785bfcd665dc4c879a2f9e58440c84
xu <>
2018-05-13 19:56:35 +0800

    获取分页数据 后缀添加

--- app/Http/Controllers/Robot/CommunityController.php
+++ app/Http/Controllers/Robot/CommunityController.php
@@ -16,40 +16,55 @@ use Illuminate\Http\Request;
 class CommunityController extends Controller
 {
     public function index(Request $request){
+        $input = $request->input();
         $where = [];
+        $sort = 'asc';
+        $act = isset($input['act']) ? $input['act'] : ">";
+        $offset = isset($input['offset']) ? $input['offset'] : 0;
+        $type = isset($input['type']) ? $input['type'] : 2;
+        $r_uid = isset($input['r_uid']) ? $input['r_uid'] : 0;
         //1 朋友可见  2 广场圈
-        if ($request->type == 1){
-            $where['r_uid'] = $request->r_uid;
+        if ($type == 1 && $r_uid > 0){
+            $where['r_uid'] = $r_uid;
             $where['type'] = 1;
         }else{
             $where['type'] = 2;
         }
-        $bool = isset($request->input()['limit']);
+        $bool = isset($input['limit']);
         if (!$bool){
-            $limit = 10;
+            $limit = 8;
         }else{
             $limit = $request->limit;
         }
+
+        if ($offset == -1){
+            $offset = 0;
+            $sort = "desc";
+        }
+
+        if ($offset > 0 && $act == "pre"){
+            $act = "<";
+            $sort = "desc";
+        }else{
+            $act = ">";
+        }
+
         $list = \DB::table('robot_article')
             ->select("robot_article.*","NName as name","HeadUrl as head_image")
             ->leftJoin("RUserBase",'r_uid','=','UId')
             ->where($where)
-            ->paginate($limit);
+            ->where('id',$act,$offset)
+            ->limit($limit)
+            ->orderBy('id',$sort)
+            ->get();
         foreach ($list as $item){
             $item->created_at = intval(strtotime($item->created_at));
             $item->updated_at = intval(strtotime($item->updated_at));
             $item->like = intval($item->like);
             $item->time_length = intval($item->time_length);
         }
-        $list_arr = [];
-        if ($list->total() > 0){
-            foreach (collect($list->items())->sortByDesc('id') as $v){
-                $list_arr[] = $v;
-            }
-        }
-        $data['list'] = $list_arr;
-        $data['page'] = isset($request->input()['page']) ? intval($request->page) : 1;
-        $data['total'] = $list->total();
+        $data['list'] = $list;
+        $data['total'] = $list->count();
         $data['limit'] = intval($limit);
         return response()->json($data);
     }
@@ -60,18 +75,21 @@ class CommunityController extends Controller
             $voice_path = base_path('public/upload/voice');
             if ($request->hasFile('image')){
                 $name =  $request->file('image')->getClientOriginalName();
+                $ext =  $request->file('image')->getClientOriginalExtension();
                 $image = 'upload/images/img_'.time().$name;
-                $request->file('image')->move($img_path,'img_'.time().$name);
+                $request->file('image')->move($img_path,'img_'.time().$name.".".$ext);
             }
             if ($request->hasFile('voice')){
                 $name =  $request->file('voice')->getClientOriginalName();
+                $ext =  $request->file('voice')->getClientOriginalExtension();
                 $voice = 'upload/voice/voice_'.time().$name;
-                $request->file('voice')->move($voice_path,'voice_'.time().$name);
+                $request->file('voice')->move($voice_path,'voice_'.time().$name.".".$ext);
             }
             if ($request->hasFile('thumbnail')){
                 $name =  $request->file('thumbnail')->getClientOriginalName();
+                $ext =  $request->file('thumbnail')->getClientOriginalExtension();
                 $thumbnail = 'upload/images/thumbnail_'.time().$name;
-                $request->file('thumbnail')->move($img_path,'thumbnail_'.time().$name);
+                $request->file('thumbnail')->move($img_path,'thumbnail_'.time().$name.".".$ext);
             }
             if (isset($request->input()['time_length'])){
                 $time = $request->input()['time_length'];
